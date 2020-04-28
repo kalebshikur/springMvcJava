9534d099941035a0c64822699a97617f88bda53e
kalebshikur <kalebm1sales@gmail.com>
2020-04-28 19:10:29 -0400

    first commit

---
+++ src/main/java/com/kaleb/web/MyConfig.java
@@ -0,0 +1,13 @@
+package com.kaleb.web;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.kaleb.web")
+public class MyConfig extends WebMvcConfigurerAdapter {
+
+}
